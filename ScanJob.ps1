c4a750928b38e587d980e37cb5a47facf0983366
Tuong Nguyen <stranger0411@gmail.com>
2020-07-03 13:56:27 +1000

    Add released script for scan on any folder

---
+++ ScanJob.ps1
@@ -0,0 +1,52 @@
+﻿cd C:\C@d3\ScanFolder
+#Get current .net version used
+$PSVersionTable.CLRVersion
+
+#Define var
+$CurrentDir = $(get-location).Path
+$AlphaFSPath = $CurrentDir + "\AlphaFS.2.2.6.0\Lib\Net40\AlphaFS.dll"
+Import-Module -Name $AlphaFSPath
+$testFolder = $CurrentDir + "\TestFol"
+$scanFolder = "\\pbnenas01\Software"
+
+#Load the Function
+. .\Get-AlphaFSChildItem.ps1
+
+#Check if the func is available
+#ls function: | where { $_.Name -eq "Get-AlphaFSChildItem" }
+
+#Scan the test folder
+Get-ChildItem -Path $testFolder -recurse -Exclude Windows -Directory 
+
+#Scan the test folder with long name
+$dirs = [Alphaleonis.Win32.Filesystem.Directory]::EnumerateFileSystemEntries($testFolder, '*', [System.IO.SearchOption]::AllDirectories) 
+
+| select-object FullName, LastWriteTime | export-csv -notypeinformation -delimiter '|' -path "$CurrentDir\output.csv"
+
+ForEach ($dir in $dirs) {
+      
+}
+
+
+#Get-ChildItem -Path $testFolder | Select-Object FullName
+
+$dirs = Get-Childitem -Path $testFolder -Exclude Windows -Directory
+
+
+
+ForEach ($dir in $dirs) {
+    $newDir = "$dir\Documents\CIP\"
+    $testPath = Test-Path -Path $newDir
+        If ($testPath -eq $True) {
+            Remove-Item -Path $newDir -Include st_* -Exclude STAC,upload -Recurse -Force -Verbose
+        }
+}
+
+write-host dirs
+Get-AlphaFSChildItem -path $testFolder -recurse | select-object FileName, FullPath, Fullsize, CreationTime, LastWriteTime | export-csv -notypeinformation -delimiter '|' -path "$CurrentDir\output.csv"
+
+$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
+Write-host "My directory is $dir"
+
+write-host $PSScriptRoot
---
+++ release.ps1
@@ -0,0 +1,33 @@
+﻿#Open shell on current folder
+#Get current .net version used
+$PSVersionTable.CLRVersion
+
+#Define var
+cd $PSScriptRoot
+$AlphaFSPath = $PSScriptRoot + "\AlphaFS.2.2.6.0\Lib\Net40\AlphaFS.dll"
+Import-Module -Name $AlphaFSPath
+
+$testFolder = $PSScriptRoot + "\TestFolder"
+
+#Load the Folder need to be scaned from param
+$scanFolder = $args[0]
+if ($null -eq $scanFolder){
+    $scanFolder = read-host -Prompt "Please enter Directory need to be scaned"
+    if (0 -eq $scanFolder.Length){
+        $scanFolder = $testFolder
+    }
+}
+
+
+#Load the Function
+. .\Get-AlphaFSChildItem.ps1
+
+Try {
+        Write-Host "Scan $scanFolder STARTED" -foregroundcolor Green
+	    Get-AlphaFSChildItem -path $scanFolder -recurse | select-object FileName, FullPath, Fullsize, CreationTime, LastWriteTime | export-csv -notypeinformation -delimiter '|' -path "$PSScriptRoot\output.csv"
+        Write-Host "Scan $scanFolder COMPLETED and EXPORTED!" -foregroundcolor Yellow
+	}
+Catch [System.UnauthorizedAccessException] {
+        #Report exception.
+        Write-Host "Scan $scanFolder FAILED" -foregroundcolor Red
+	}
